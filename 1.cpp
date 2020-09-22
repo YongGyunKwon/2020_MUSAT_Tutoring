689bbc13c15ed9bb45f8d0016a0af8206343af2d
YongGyunKwon <ygkwon50@gmail.com>
2020-09-23 01:44:20 +0900

    conflict?

--- 1.cpp
+++ 1.cpp
@@ -6,11 +6,5 @@ int main ()
     cout<<"Lee Ba da"<<endl;
     cout<<"Son Tottenham"<<endl;
 
-<<<<<<< .merge_file_a14340
-    cout<<"Ali"<<endl;
-    cout<<"Deli"<<endl;
-}
-=======
     cout<<"Bale";
 }
->>>>>>> .merge_file_a15104
